64e27e4d42986ae276e3e44e7dfa39e16ee4f550
Ilya <fominiv10@gmail.com>
2022-09-26 21:43:32 +0300

    solved the sixtieth  task

---
+++ Task 60/Program.cs
@@ -0,0 +1,33 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+void OutputThreeDimensionalArray (int [,,] array)
+{
+     for (int i = 0; i < array.GetLength(2); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                Console.Write($"{array[j,k,i]}({j},{k},{i}) ");
+            }
+        Console.WriteLine();    
+        }    
+    }
+}
+
+
+int [,,] array =
+{
+    {
+        {11, 44, 76},
+        {65, 36, 74}
+    },
+
+    {
+        {52, 33, 12},
+        {25, 99, 88}
+    }
+};
+
+OutputThreeDimensionalArray (array);
